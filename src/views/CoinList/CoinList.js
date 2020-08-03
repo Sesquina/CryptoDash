12d5fa57c2021ff3de1f86b2dcec6da5d7ad002e
Samira Esquina <samiraesquina@gmail.com>
2020-08-03 15:41:17 -0400

    Fixed a SERIOUS bug

--- src/views/CoinList/CoinList.js
+++ src/views/CoinList/CoinList.js
@@ -1,16 +1,38 @@
-import React from 'react';
+// Import React and Component
+import React, { Component } from 'react';
+// Import CSS from App.css
+import './CoinList.css';
+// Import the Today component to be used below
+import Today from './Today'
+// Import the History component to be used below
+import History from './History'
 
-
-
-
-const CoinList = props => {
-  return (
-    <div className="coinlist">
-      <h3>Coin List</h3>
-      <p> These are the top Coins</p>
-    
-    </div>
-  );
-};
+class CoinList extends Component {
+  render() {
+    return (
+      <div className="">
+          <div className="topheader">
+              <header className="container">
+                  <nav className="navbar">
+                      <div className="navbar-brand">
+                          <span className="navbar-item">Coin List</span>
+                      </div>
+                    
+                  </nav>
+              </header>
+          </div>
+          <section className="results--section">
+              <div className="container">
+                  <h1>Here's realtime price information about<br></br> BTC, ETH and LTC.</h1>
+              </div>
+              <div className="results--section__inner">
+                  <Today />
+                  <History />
+              </div>
+          </section>
+      </div>
+    );
+  }
+}
 
 export default CoinList;
--- src/views/CoinList/Styles.js
+++ src/views/CoinList/Styles.js
@@ -27,4 +27,4 @@ export const fontSize1 = 'font-size: 1.5em;';
 export const fontSize2 = 'font-size: 1.0em';
 export const fontSize3 = 'font-size: .75em';
 
-export const textAlignCenter = 'text-align: center;';
+// export const textAlignCenter = 'text-align: center;';
